7397d330f715dfd0755b26cb3d6acbf72f879c1c
Niyazov0607 <shohjahonniyazov89@gmail.com>
2024-11-30 00:17:40 +0500

    animation

---
+++ js/index.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        container: {
+            center: true,
+            width: {
+                desktop: "1440px",
+            },
+            padding: {
+                DEFAULT: "24px",
+                desktop: "0",
+            },
+        },
+        screens: {
+            desktop: "1320px",
+        },
+    },
+};
